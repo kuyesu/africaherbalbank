af8433f0c6e8f650e7815a9803e8eb525c85a7a4
kuyesu <2019bce028@std.must.ac.ug>
2021-09-06 16:05:07 +0100

    h

--- core/settings/base.py
+++ core/settings/base.py
@@ -235,7 +235,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "core"
